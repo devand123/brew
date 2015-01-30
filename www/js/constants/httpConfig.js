6f6e177a30d5546ebd5d4a52d19b03f352885eb1
devand123@gmail.com <devand123@gmail.com>
2015-01-30 15:16:03 -0500

    Initial commit

--- www/js/constants/httpConfig.js
+++ www/js/constants/httpConfig.js
@@ -1,4 +1,5 @@
 angular.module('brew')
 	.constant('httpConfig', {
-		baseURL: 'http://localhost:3000/'
+		// baseURL: 'http://localhost:3000/'
+		baseURL: 'https://test-brew-app.herokuapp.com/'
 	});
